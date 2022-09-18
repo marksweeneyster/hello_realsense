5162cbadf647808a2fd4d14f96033aa4d6f7fe02
Mark Sweeney <marksweeneyster@gmail.com>
2022-09-18 16:39:16 -0600

    some actual (BUILDS-ON-MY-MACHINE) code

---
+++ frame_rate.cpp
@@ -0,0 +1,34 @@
+//
+// Created by mark on 9/18/22.
+//
+#include <iostream>
+#include <chrono>
+#include <librealsense2/rs.hpp>
+[[noreturn]]
+int main() {
+    std::cout << "Hello, World!\n\n";
+
+    using Clock = std::chrono::steady_clock;
+    using std::chrono::time_point;
+    using std::chrono::duration_cast;
+    using std::chrono::milliseconds;
+    using namespace std::literals::chrono_literals;
+
+    std::string time_units = "\u03BCs";
+
+    // Create a Pipeline - this serves as a top-level API for streaming and processing frames
+    rs2::pipeline p;
+
+    // Configure and start the pipeline
+    p.start();
+
+    while (true)
+    {
+        time_point<Clock> start = Clock::now();
+        // Block program until frames arrive
+        rs2::frameset frames = p.wait_for_frames();
+        time_point<Clock> stop = Clock::now();
+        auto diff = duration_cast<std::chrono::microseconds>(stop - start);
+        std::cout << '\t' << diff.count() << time_units << " \n";
+    }
+}
---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <librealsense2/rs.hpp>
+[[noreturn]]
+int main() {
+    std::cout << "Hello, World!\n\n";
+
+    // Create a Pipeline - this serves as a top-level API for streaming and processing frames
+    rs2::pipeline p;
+
+    // Configure and start the pipeline
+    p.start();
+
+    while (true)
+    {
+        // Block program until frames arrive
+        rs2::frameset frames = p.wait_for_frames();
+
+        // Try to get a frame of a depth image
+        rs2::depth_frame depth = frames.get_depth_frame();
+
+        // Get the depth frame's dimensions
+        auto width = depth.get_width();
+        auto height = depth.get_height();
+
+        // Query the distance from the camera to the object in the center of the image
+        float dist_to_center = depth.get_distance(width / 2, height / 2);
+        //auto h = depth.get_height();
+        //auto w = depth.get_width();
+        // Print the distance
+        std::cout << "The camera is facing an object " << dist_to_center << " meters away \r";
+        //std::cout << "frame size: (" << h << "," << w << ") \r";
+    }
+}
